50e86b4cca5c9dd8464d53221ff1b61fdbf73a79
Kunal Choudhary <kunal.choudhary@highlands.in>
2022-01-02 21:20:45 +0530

    Practice of Variables and Data Types

--- src/com/basicofjava/Main.java
+++ src/com/basicofjava/Main.java
@@ -6,13 +6,15 @@ public class Main {
     public static void main(String[] args) {
     // Taking Input in Java
 //    Scanner sc = new Scanner(System.in);
-    // String name = sc.next(); // this will only take one input and don't take any other spaces i.e Kunal Choudhary
+//    String name = sc.next(); // this will only take one input and don't take any other spaces i.e Kunal Choudhary
 //    String name = sc.nextLine();
 //    System.out.print(name);
+
 //    More input methods
 //        nextInt()
 //        nextFloat()
 //        nextDouble()
+
 	// output
     // System.out.println("*");
     // System.out.println("**");
@@ -38,11 +40,33 @@ public class Main {
    // int answer = (a * b) / (a - b);
    // System.out.print(answer);
 //        Take two variable 'a' & 'b' and print their sum
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int sum = a + b;
-        System.out.println(sum);
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int sum = a + b;
+//        System.out.println(sum);
 
+//        Make a program that takes the radius of a circle as input, calculates
+//        its radius and area and prints it as output to the user2.
+//        Make a program that takes the radius of a circle as
+//        input, calculates its radius and area and prints it as output to the user
+//        Scanner sc = new Scanner(System.in);
+//        float radius = sc.nextFloat();
+//        double pie = 3.14;
+//        double area = (pie) * (radius * radius);
+//        System.out.println(area);
+//      Print a table that is given by User
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+//        System.out.println(num * 1);
+//        System.out.println(num * 2);
+//        System.out.println(num * 3);
+//        System.out.println(num * 4);
+//        System.out.println(num * 5);
+//        System.out.println(num * 6);
+//        System.out.println(num * 7);
+//        System.out.println(num * 8);
+//        System.out.println(num * 9);
+//        System.out.println(num * 10);
     }
 }
