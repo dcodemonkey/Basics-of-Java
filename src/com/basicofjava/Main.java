a8a2ced79978068fae846f73c5fc011f0c3616b1
Kunal Choudhary <kunal.choudhary@highlands.in>
2022-01-02 18:20:25 +0530

    Variables in Java

---
+++ src/com/basicofjava/Main.java
@@ -0,0 +1,22 @@
+package com.basicofjava;
+
+public class Main {
+    public static void main(String[] args) {
+	// output
+    // System.out.println("*");
+    // System.out.println("**");
+    // System.out.println("***");
+    // System.out.println("****");
+    // System.out.println("*****");
+
+    // System.out.println("*\n**");
+
+    // Variables
+        int a = 10;
+        int b = 25;
+        int sum = a + b;
+        //System.out.println(sum);
+
+
+    }
+}
