c048f77639339752f5bc7d1bdd2d915f33f5488f
Kunal Choudhary <kunal.choudhary@highlands.in>
2022-01-02 19:09:04 +0530

    Working with Inputs in Java

--- src/com/basicofjava/Main.java
+++ src/com/basicofjava/Main.java
@@ -1,7 +1,18 @@
 package com.basicofjava;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+    // Taking Input in Java
+//    Scanner sc = new Scanner(System.in);
+    // String name = sc.next(); // this will only take one input and don't take any other spaces i.e Kunal Choudhary
+//    String name = sc.nextLine();
+//    System.out.print(name);
+//    More input methods
+//        nextInt()
+//        nextFloat()
+//        nextDouble()
 	// output
     // System.out.println("*");
     // System.out.println("**");
@@ -12,11 +23,26 @@ public class Main {
     // System.out.println("*\n**");
 
     // Variables
-        int a = 10;
-        int b = 25;
+   // int a = 10;
+   // int b = 25;
+   // int sum = a + b;
+   // System.out.println(sum);
+   // int diff = b - a ;
+   // System.out.println(diff);
+   // int mul = a*b;
+   // System.out.print(mul);
+
+   // Quiz to calcuate a x b / a - b; where a = 10, b = 5;
+   // int a = 10;
+   // int b = 5;
+   // int answer = (a * b) / (a - b);
+   // System.out.print(answer);
+//        Take two variable 'a' & 'b' and print their sum
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         int sum = a + b;
         System.out.println(sum);
-
 
     }
 }
