f3247829c46ffeecceab7eda605c973cec782dd5
Kunal Choudhary <kunal.choudhary@highlands.in>
2022-01-05 23:17:59 +0530

    Sum, Product and Factorial of a number

--- src/com/basicofjava/function.java
+++ src/com/basicofjava/function.java
@@ -4,16 +4,37 @@ import java.util.*;
 
 public class function {
 
-  public static void printMyName(String name) {
-    System.out.println(name);
+  // public static int calculateSum(int a, int b) {
+  //   int sum  = a + b;
+  //   return sum;
+  // }
+
+  // public static int calculateProduct(int a, int b) {
+  //   return a * b;
+  // }
+
+  public static void calculateFactorial(int n) {
+    // loop
+    int factorial = 1;
+
+    for(int i=n; i>=1; i--){
+      factorial = factorial * i;
+    }
+    System.out.println(factorial);
     return;
   }
 
   public static void main(String args[]) {
    Scanner sc = new Scanner(System.in);
-   String name = sc.next();
+   int n = sc.nextInt();
+   calculateFactorial(n);
 
-   printMyName(name); // Calling function
+  //  int sum  = calculateSum(a, b);
+  //  System.out.println("Sum of 2 numbers is: " + sum);
+
+  //  System.out.println("Sum of 2 numbers is: " + calculateProduct(a,b));
+
+
   }
   
 }
